40dd831880372648d1de863df8e7d3e19fce52c3
Paulo Vidal <paulo.vidal@bitfy.app>
2023-08-01 23:15:36 -0300

    chore: initial commit

---
+++ test/Faucet.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {Faucet} from "../src/Faucet.sol";
+
+contract FaucetTest is Test {}
